1db96b6fdfe5544f22dac74f0c37076139316141
yahutahugg <92901932+yahutahugg@users.noreply.github.com>
2022-03-18 15:55:54 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector("nav");
-
-window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 60) {
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
-});
